6cf9afa848c77b25093b36ad64de65b50e50bf79
mikejunt <mikejunt@gmail.com>
2020-03-30 14:22:15 -0500

    creates components and imports modules

---
+++ src/app/instructors/instructor-landing/instructor-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-landing',
+  templateUrl: './instructor-landing.component.html',
+  styleUrls: ['./instructor-landing.component.scss']
+})
+export class InstructorLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
