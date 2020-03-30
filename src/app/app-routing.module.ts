604cb79fde8c16275d1e65ee00e36a3f1d0b24c4
mikejunt <mikejunt@gmail.com>
2020-03-30 16:07:03 -0500

    reconfigures the basic route scaffold

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './shared/welcome/welcome.component';
-import { StudentLandingComponent } from './students/student-landing/student-landing.component';
-import { InstructorLandingComponent } from './instructors/instructor-landing/instructor-landing.component';
-import { AdminLandingComponent } from './admin/admin-landing/admin-landing.component';
+import { AdminLandingComponent } from './admin/admin-landing.component';
+import { QuizLandingComponent } from './quiz/quiz-landing.component';
+import { LessonLandingComponent } from './lesson/lesson-landing.component';
 
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'student', component: StudentLandingComponent },
-  { path: 'instructor', component: InstructorLandingComponent },
+  { path: 'quiz', component: QuizLandingComponent },
+  { path: 'lesson', component: LessonLandingComponent },
   { path: 'admin', component: AdminLandingComponent },
   { path: '', redirectTo: 'welcome' }
 ];
