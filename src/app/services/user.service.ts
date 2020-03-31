49ceb56a61cb41037edacaab152a3599049f6881
Jblobaum <justin.blobaum@midlandu.edu>
2020-03-31 13:16:27 -0500

    set up user service to log the user info from auth

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  user$: Observable<Object>;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService
+    ) { }
+
+    setUserData(){
+      this.user$ = this.auth.getUser$()
+      this.user$.subscribe(res=>console.log(res));
+      
+    }
+
 }
