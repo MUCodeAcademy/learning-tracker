a45af1e7892b25d8b4d9894f87bd562b7ae1b520
Jblobaum <justin.blobaum@midlandu.edu>
2020-03-31 11:17:49 -0500

    changed name and regenerated user guard

--- src/app/guards/user.guard.spec.ts
+++ src/app/guards/user.guard.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { User.GuardGuard } from './user.guard.guard';
+import { UserGuard } from './user.guard';
 
-describe('User.GuardGuard', () => {
-  let guard: User.GuardGuard;
+describe('UserGuard', () => {
+  let guard: UserGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(User.GuardGuard);
+    guard = TestBed.inject(UserGuard);
   });
 
   it('should be created', () => {
