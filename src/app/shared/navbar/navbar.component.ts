b4c1cee52515041c963c71ad46d8dc4f4df34e1a
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-01 09:57:02 -0500

    added log out button functionality

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   logout(){
-    console.log('Logout Works')
+    this.auth.logout()
   }
 
   ngOnInit(): void {
