393ce7133329d00ff575691740bc733b2546b50f
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-02 10:58:43 -0500

    getusers, students, instructors, studentsbycohort

---
+++ server/models/user.model.js
@@ -0,0 +1,42 @@
+const { client, pool } = require('../config/postgres.conf')
+
+
+function getAllUsers(response, request) {
+    pool.query("SELECT * FROM users").then(res => {
+        if (res.rows.length === 0) {
+            return response.send({ success: false, msg: "No users found." })
+        }
+        else return response.send({ success: true, msg: "Data retrieved.", data: res.rows })
+    })
+    .catch(err => console.log(err))
+}
+
+function getAllStudents(response, request) {
+    pool.query("SELECT * FROM users JOIN role ON user.role_id = role.id WHERE role.id = 3").then(res => {
+        if (res.rows.length === 0) {
+            return response.send({ success: false, msg: "No students found." })
+        }
+        else return response.send({ success: true, msg: "Data retrieved.", data: res.rows })
+    })
+    .catch(err => console.log(err))
+}
+
+function getAllStudentsbyCohort(response, request) {
+    pool.query("SELECT * FROM user JOIN role ON user.role_id = role.id JOIN cohort_to_student ON user.id = cohort_to_student.user_id JOIN cohort ON cohort_to_student.cohort_id = cohort.id WHERE cohort_name = $1"), name.then(res => {
+        if (res.rows.length === 0) {
+            return response.send({ success: false, msg: "No students found." })
+        }
+        else return response.send({ success: true, msg: "Data retrieved.", data: res.rows })
+    })
+    .catch(err => console.log(err))
+}
+
+function getAllInstructors(response, request) {
+    pool.query("SELECT * FROM users JOIN role ON user.role_id = role.id WHERE role.id = 1").then(res => {
+        if (res.rows.length === 0) {
+            return response.send({ success: false, msg: "No instructors found." })
+        }
+        else return response.send({ success: true, msg: "Data retrieved.", data: res.rows })
+    })
+    .catch(err => console.log(err))
+}
