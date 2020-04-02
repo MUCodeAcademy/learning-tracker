2e0915383156851187abffdaf4c9cc9a28187697
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-02 15:31:47 -0500

    created topic retention

---
+++ server/models/topic.retention.model.js
@@ -0,0 +1,24 @@
+const { client, pool } = require('../config/postgres.conf')
+
+
+
+function addRetentionByTopic(){
+
+}
+
+function getAllRatingsByTopic(){
+
+}
+
+function getAllRatingsByStudent(){
+
+}
+
+function getAllRatingsByCohort(){
+    
+}
+
+function editRetention(){
+
+}
+
