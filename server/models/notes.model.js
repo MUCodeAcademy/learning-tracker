a4c74fb3b655061659469e13f0b625b388556cf1
mikejunt <mikejunt@gmail.com>
2020-04-03 14:35:34 -0500

    implements notes procs, adds to docs

--- server/models/notes.model.js
+++ server/models/notes.model.js
@@ -20,7 +20,7 @@ export function getAllNotes(response, request) {
 }
 
 export function getAllNotesByStudent(response, request) {
-    let student_id = [request.body.student_id]
+    let student_id = [request.params.id]
     pool.query("SELECT * FROM notes WHERE student_id = $1", student_id).then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No notes found." })
@@ -30,9 +30,9 @@ export function getAllNotesByStudent(response, request) {
     .catch(err => console.log(err))
 }
 
-export function getAllNotesByTopic(response, request) {
-    let topic_id = [req.body.topic_id]
-    pool.query("SELECT * FROM notes WHERE topic_id = $1", topic_id).then(res => {
+export function getAllNotesByCohort(response, request) {
+    let id = [request.params.id]
+    pool.query("SELECT data FROM note_get_all_by_cohort_id($1)", topic_id).then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No notes found." })
         }
@@ -41,9 +41,9 @@ export function getAllNotesByTopic(response, request) {
     .catch(err => console.log(err))
 }
 
-export function getAllNotesByCohort(response, request) {
-    let cohort_id = [req.body.cohort_id]
-    pool.query("SELECT * FROM notes WHERE cohort_id = $1", cohort_id).then(res => {
+export function getAllNotesByTopic(response, request) {
+    let cohort_id = [request.body.topicid, request.body.cohortid]
+    pool.query("SELECT data FROM note_get_all_by_topic_id($1,$2)", cohort_id).then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No notes found." })
         }
@@ -61,7 +61,7 @@ export function updateNote(response, request) {
 }
 
 export function deleteNote(response, request) {
-    let id = [request.body.id];
+    let id = [request.params.id];
     pool.query("DELETE FROM notes WHERE notes.id = $1", id, (err, result, field) => {
         if (err) { return console.log("Error on query", err.stack) }
         return response.send({ success: true, msg: "Note deleted." })
--- server/routes/notes.routes.js
+++ server/routes/notes.routes.js
@@ -14,19 +14,19 @@ router.get('/student/:id', (req, res) => {
     Models.getAllNotesByStudent(res, req);
 });
 
-router.get('/topic/:id', (req, res) => {
-    Models.getAllNotesByTopic(res, req);
-});
-
 router.get('/cohort/:id', (req, res) => {
     Models.getAllNotesByCohort(res, req);
+});
+
+router.post('/topic/', (req, res) => {
+    Models.getAllNotesByTopic(res, req);
 });
 
 router.put('/update', (req, res) => {
     Models.updateNote(res,req);
 });
 
-router.delete('/delete', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
     Models.deleteNote(res,req);
 });
 
