1e1b11b3671602fbd49a6dbb1596f50e875c7a6e
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-03 11:07:39 -0500

    fixed variables with req.body instead of res

--- server/models/topic.retention.model.js
+++ server/models/topic.retention.model.js
@@ -3,10 +3,10 @@ import {client, pool } from '../config/postgres.conf'
 
 
 export function addStudentRetentionByTopic(response, request){
-    let topicId = [response.topicId]
-    let studentId = [response.Id]
-    let lessonId = [response.lessionId]
-    let newRetention = [response.newRetention]
+    let topicId = [request.body.topicId]
+    let studentId = [request.body.Id]
+    let lessonId = [request.body.lessionId]
+    let newRetention = [request.body.newRetention]
     pool.query("INSERT INTO topic_retention (id, student_id, lesson_id, instructor_id, student_retention_rating, teacher_retention_rating, topic_id) VALUES (DEFAULT, $1, $2, DEFAULT, $3, DEFAULT, $4)", studentId, lessonId, newRetention, topicId, (err, results) => {
         if (err) {
             return res.send({ success: false, err: err });
@@ -16,10 +16,10 @@ export function addStudentRetentionByTopic(response, request){
 }
 
 export function addInstructorRetentionByTopic(response, request){
-    let topicId = [response.topicId]
-    let instructorId = [response.Id]
-    let lessonId = [response.lessionId]
-    let newRetention = [response.newRetention]
+    let topicId = [request.body.topicId]
+    let instructorId = [request.body.Id]
+    let lessonId = [request.body.lessionId]
+    let newRetention = [request.body.newRetention]
     pool.query("INSERT INTO topic_retention (id, student_id, lesson_id, instructor_id, student_retention_rating, teacher_retention_rating, topic_id) VALUES (DEFAULT, DEFAULT, $1, $2, DEFAULT, $3, $4)", lessonId, instructorId, newRetention, topicId, (err, results) => {
         if (err) {
             return res.send({ success: false, err: err });
@@ -29,7 +29,7 @@ export function addInstructorRetentionByTopic(response, request){
 }
 
 export function getAllRatingsByTopic(response, request){
-    let studentId = [response.studentId]
+    let studentId = [request.body.studentId]
     pool.query("SELECT * FROM topic_retention WHERE topic_retention.student.id = $1"), studentId.then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No ratings found." })
@@ -40,7 +40,7 @@ export function getAllRatingsByTopic(response, request){
 }
 
 export function getAllRatingsByStudent(response, request){
-    let studentId = [response.studentId]
+    let studentId = [request.body.studentId]
     pool.query("SELECT * FROM topic_retention WHERE topic_retention.student.id = $1"), studentId.then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No ratings found." })
@@ -52,9 +52,9 @@ export function getAllRatingsByStudent(response, request){
 }
 
 export function editStudentRetentionByTopic(response, request){
-    let topicId = [response.topicId]
-    let id = [response.id]
-    let newRetention = [response.newRetention]
+    let topicId = [request.body.topicId]
+    let id = [request.body.id]
+    let newRetention = [request.body.newRetention]
     pool.query("UPDATE topic_retention SET topic_retention.student_retention = $1 WHERE topic_retention.student_id = $2 AND topic_retention.topic_id = $3", newRetention,id, topicId, (err, result, field) => {
         if (err) { return console.log("Error on query", err.stack) }
         return response.send({ success: true, msg: "Retention updated." })
@@ -62,9 +62,9 @@ export function editStudentRetentionByTopic(response, request){
 }
 
 export function editInstructorRetentionByTopic(response, request){
-    let topicId = [response.topicId]
-    let id = [response.id]
-    let newRetention = [response.newRetention]
+    let topicId = [request.body.topicId]
+    let id = [request.body.id]
+    let newRetention = [request.body.newRetention]
     pool.query("UPDATE topic_retention SET topic_retention.teacher_retention = $1 WHERE topic_retention.instructor_id = $2 AND topic_retention.topic_id = $3", newRetention,id, topicId, (err, result, field) => {
         if (err) { return console.log("Error on query", err.stack) }
         return response.send({ success: true, msg: "Retention updated." })
