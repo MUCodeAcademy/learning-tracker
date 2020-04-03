9ddc1b416a3be662a5ea5e97800a5bfbe4733a7a
mikejunt <mikejunt@gmail.com>
2020-04-03 14:51:46 -0500

    lol babel

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const https = require("https");
 const fs = require("fs");
 import cohorts from './server/routes/cohort.routes'
 import questions from './server/routes/questions.routes'
+import lessons from './server/routes/lessons.routes'
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.use(express.static(__dirname + "/dist"));
 
-app.use('/api/cohort/', cohorts);
-app.use('/api/question/', questions);
+app.use('/api/cohorts/', cohorts);
+app.use('/api/questions/', questions);
+app.use('/api/lessons', lessons)
 
 // Allows for angular routing to take precedent
 app.get("*", (req, res) =>
