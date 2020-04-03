8171873d96aa5cef36f6b1bf15e3e9ec92899455
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-03 17:27:21 -0500

    bug fixed and tested note route and model

--- server/models/notes.model.js
+++ server/models/notes.model.js
@@ -21,7 +21,7 @@ export function getAllNotes(response, request) {
 
 export function getAllNotesByStudent(response, request) {
     let student_id = [request.params.id]
-    pool.query("SELECT * FROM notes WHERE student_id = $1", student_id).then(res => {
+    pool.query("SELECT * FROM note WHERE user_id = $1", student_id).then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No notes found." })
         }
@@ -32,7 +32,7 @@ export function getAllNotesByStudent(response, request) {
 
 export function getAllNotesByCohort(response, request) {
     let id = [request.params.id]
-    pool.query("SELECT data FROM note_get_all_by_cohort_id($1)", topic_id).then(res => {
+    pool.query("SELECT data FROM note_get_all_by_cohort_id($1)", id).then(res => {
         if (res.rows.length === 0) {
             return response.send({ success: false, msg: "No notes found." })
         }
@@ -62,7 +62,7 @@ export function updateNote(response, request) {
 
 export function deleteNote(response, request) {
     let id = [request.params.id];
-    pool.query("DELETE FROM notes WHERE notes.id = $1", id, (err, result, field) => {
+    pool.query("DELETE FROM note WHERE note.id = $1", id, (err, result, field) => {
         if (err) { return console.log("Error on query", err.stack) }
         return response.send({ success: true, msg: "Note deleted." })
     })
