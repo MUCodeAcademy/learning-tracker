81f834772b942198a79ae07d1bd67540679a019c
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-03 10:59:26 -0500

    basic route skeleton

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import * as Models from '../models/user.model'
 
-router.get('/all', (req, res) =>{
+router.get('/users', (req, res) =>{
     Models.getAllUsers(res, req);
 });
 
@@ -10,6 +10,30 @@ router.get('/students', (req, res) =>{
     Models.getAllStudents(res, req);
 });
 
-router.get('/students/:cohortid', (req, res) =>{
+router.get('/students/cohort/:id', (req, res) =>{
     Models.getAllUsers(res, req);
+});
+
+router.get('/instructors', (req, res) =>{
+    Models.getAllInstructors(res, req);
+});
+
+router.get('/instructors/cohort/:id', (req, res) =>{
+    Models.getAllInstructorsbyCohort(res, req);
+});
+
+router.get('/admin', (req, res) =>{
+    Models.getAllAdmin(res, req);
+});
+
+router.delete('/users', (req, res) =>{
+    Models.deleteUser(res, req)
+});
+
+router.post('/users', (req, res) =>{
+    Models.editUser(req, res)
+});
+
+router.get('/users/new', (req, res) =>{
+    Models.getUserInfo(res, req)
 });
