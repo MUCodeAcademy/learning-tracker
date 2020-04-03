b98ef5440da57e40b45b4dde322b3f7cafaddbe8
Jacqueline Mezick <jmezick@Jacquelines-MacBook-Air.local>
2020-04-03 12:04:01 -0500

    quiz schema

---
+++ server/models/quiz.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+let quizSchema = mongoose.Schema;
+
+let quizSchema = mongoose.Schema({
+  quizName: { type: String, required: true },
+  quizWeek: { type: Number, required: true },
+  cohort: { type: Number, required: true },
+  questions: [
+    {
+      Q: { type: String, required: true },
+      A: { type: String, required: true },
+      Choices: [String],
+      code: String
+    }
+  ]
+});
