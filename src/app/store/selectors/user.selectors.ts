4923d587e5be984ce643dbd730d3ede16649eb6d
mikejunt <mikejunt@gmail.com>
2020-04-06 16:56:47 -0500

    fixes a variety if auth0-to-db key issues

--- src/app/store/selectors/user.selectors.ts
+++ src/app/store/selectors/user.selectors.ts
@@ -1,8 +1,8 @@
 
 import { RootState } from '..';
 
-export const getUserRole = (state: RootState) => state.user.userRole
-export const getUserEmail = (state: RootState) => state.user.userEmail
-export const getUserId = (state: RootState) => state.user.userId
+export const getUserRole = (state: RootState) => state.user.currentuser.role
+export const getUserEmail = (state: RootState) => state.user.currentuser.email
+export const getUserId = (state: RootState) => state.user.currentuser.id
 export const getUserList = (state: RootState) => state.user.userList
-export const getUser = (state: RootState) => state.user
+export const getUserInfo = (state: RootState) => state.user.currentuser
