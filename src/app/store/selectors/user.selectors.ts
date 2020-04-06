a2f9cd6c872c625b76af293e8111ee2f0409d151
mikejunt <mikejunt@gmail.com>
2020-04-06 15:06:50 -0500

    sets up state for a user list

--- src/app/store/selectors/user.selectors.ts
+++ src/app/store/selectors/user.selectors.ts
@@ -4,4 +4,5 @@ import { RootState } from '..';
 export const getUserRole = (state: RootState) => state.user.userRole
 export const getUserEmail = (state: RootState) => state.user.userEmail
 export const getUserId = (state: RootState) => state.user.userId
+export const getUserList = (state: RootState) => state.user.userList
 export const getUser = (state: RootState) => state.user
