efca381929a3309399389eee724ee794533c8e14
Jacqueline Mezick <jmezick@Jacquelines-MacBook-Air.local>
2020-04-06 01:15:05 -0500

    CRUD for quizzes
    Co-authored-by: Mike Truax <mike@miketruax.com>

--- server/routes/quiz.routes.js
+++ server/routes/quiz.routes.js
@@ -1,21 +1,101 @@
 import express from "express";
 const router = express.Router();
+import Quiz from "../models/quiz.model";
 
-router.add('/addQuiz', (req, res)=>{
-    res.send("working");
+// Quiz by Id
+
+router.get("/id/:id", (req, res) => {
+  Quiz.findById(req.params.id, function(err, quiz) {
+    if (err) {
+      return res.send({
+        success: false,
+        msg: "Something went wrong, please try again later."
+      });
+    }
+    return res.send({ success: true, data: quiz });
+  });
 });
 
-router.put('/editQuiz', (req, res)=>{
-    res.send("working");
+// Quizzes By Cohort
+
+router.get("/cohort/:cohort", (req, res) => {
+  Quiz.find({ cohort: req.params.cohort }, function(err, docs) {
+    if (err) {
+      return res.send({
+        success: false,
+        msg: "Something went wrong, please try again later."
+      });
+    }
+    return res.send({ success: true, data: docs });
+  });
 });
 
-router.delete('/deleteQuiz', (req, res)=>{
-    res.send("working");
+// All Quizzes
+
+router.get("/all", (req, res) => {
+  Quiz.find(),
+    function(err, docs) {
+      if (err) {
+        return res.send({
+          success: false,
+          msg: "Something went wrong, please try again later."
+        });
+      }
+    };
+  return res.send({ success: true, data: docs });
 });
 
-router.get('/getQuiz', (req, res)=>{
-    res.send("working");
-}); 
+// Create Quiz
 
+router.post("/add", (req, res) => {
+  Quiz.insertOne({
+    quizName: "Javascript",
+    quizWeek: "Week 2",
+    cohort: "3",
+    questions: [
+      {
+        Q: "",
+        A: "",
+        Choices: ["", "", "", ""],
+        code: ""
+      }
+    ],
+    if(err) {
+      return res.send({
+        success: false,
+        msg: "Something went wrong, please try again later."
+      });
+    },
+    return: res.send({ success: true, data: docs })
+  });
+});
+// Edit Quiz
+
+router.put("/edit", (req, res) => {
+  // Quiz.findByIdAndUpdate()
+  Quiz.findByIdAndUpdate(req.params.id, function(err, quiz) {
+    if (err) {
+      return res.send({
+        success: false,
+        msg: "Something went wrong, please try again later."
+      });
+    }
+    return res.send({ success: true, data: docs });
+  });
+});
+
+// Delete Quiz
+
+router.delete("/delete/:id", (req, res) => {
+  Quiz.findByIdAndDelete(req.params.id, function(err, quiz) {
+    if (err) {
+      return res.send({
+        success: false,
+        msg: "Something went, wrong please try again later."
+      });
+    }
+    return res.send({ success: true, data, docs });
+  });
+});
 
 export default router;
