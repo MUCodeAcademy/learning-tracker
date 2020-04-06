92ada94e0428b322558b2c100f6ff7de0a402e79
mikejunt <mikejunt@gmail.com>
2020-04-06 16:38:43 -0500

    sets up dev navbar as a template for editable obj

--- src/app/store/reducers/user.reducer.ts
+++ src/app/store/reducers/user.reducer.ts
@@ -3,26 +3,26 @@ import { createReducer, on, Action } from '@ngrx/store';
 import { User } from 'src/app/interfaces/User.interface';
 
 export interface UserState {
-    userEmail: string,
-    userRole: string,
-    userFirst: string,
-    userLast: string,
-    userId: string,
+    email: string,
+    role: string,
+    first_name: string,
+    last_name: string,
+    id: string,
     userList: User[]
 };
 
 export const initialUserState: UserState = {
-    userEmail: '',
-    userRole: '4',
-    userFirst: '',
-    userLast: '',
-    userId: '',
+    email: '',
+    role: '4',
+    first_name: '',
+    last_name: '',
+    id: '',
     userList: []
 };
 
 const reducer = createReducer(initialUserState,
-    on(userActions.setUserEmail, (state, {email})=> ({...state, userEmail: email })),
-    on(userActions.setUserInfo, (state, {user}) => ({...state, userRole: user.role, userFirst: user.first_name, userLast: user.last_name, userId: user.id, userEmail: user.email})),
+    on(userActions.setUserEmail, (state, {email})=> ({...state, email: email })),
+    on(userActions.setUserInfo, (state, {user}) => ({...state, role: user.role, first_name: user.first_name, last_name: user.last_name, id: user.id, email: user.email})),
     on(userActions.setUserList, (state, {userlist}) => ({...state, userList: userlist})),
     on(userActions.clearUser, (state)=> ({...initialUserState}))
 );
