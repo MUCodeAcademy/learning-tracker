fe9b5c8eac24f5754362b4ac2201877f9f43cd80
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-06 14:01:21 -0500

    Changes Related To Cohort State Management

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -3,9 +3,11 @@ import { ActionReducerMap } from '@ngrx/store';
 
 
 export interface RootState{
+    cohort: Reducers.CohortState,
     user: Reducers.UserState,
 }
 
 export const reducers: ActionReducerMap<RootState> = {
+    cohort: Reducers.cohortReducer,
     user: Reducers.userReducer,
 }
