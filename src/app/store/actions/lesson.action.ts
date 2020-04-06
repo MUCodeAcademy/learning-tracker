c0fc153f76b33edb9c358fb4797fe9e789301250
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-06 11:13:49 -0500

    made interface keys optional and started actions

--- src/app/store/actions/lesson.action.ts
+++ src/app/store/actions/lesson.action.ts
@@ -1,1 +1,4 @@
 import {createAction, props} from '@ngrx/store';
+import { Lesson } from '../../interfaces/lesson.interface'
+
+export const 
--- src/app/store/reducers/lesson.reducer.ts
+++ src/app/store/reducers/lesson.reducer.ts
@@ -2,9 +2,25 @@ import * as lessonActions from '../actions/lesson.action'
 import { createReducer, on, Action } from '@ngrx/store';
 
 export interface LessonState {
-    userEmail: string,
-    userRole: string,
-    userFirst: string,
-    userLast: string,
-    userId: string
-};
+    id: string,
+    cohort_id: string,
+    topic_id: string,
+    lesson_title: string,
+    week_number: number,
+    day: string,
+    last_edit: string
+}
+
+export const initialLessonState: LessonState = {
+    id: '',
+    cohort_id: '',
+    topic_id: '',
+    lesson_title: '',
+    week_number: 0,
+    day: '',
+    last_edit: ''
+}
+
+const reducer = createReducer(initialLessonState,
+    on()
+    )
