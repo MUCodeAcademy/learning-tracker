91c010b67297eca74a0be1833b35b03e6a95a80f
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-07 12:20:55 -0500

    Changes Related To Lesson Service

--- src/app/services/lesson.service.ts
+++ src/app/services/lesson.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Lesson } from '../interfaces/lesson.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllLessons() {
+    return this.http.get("/api/lessons/all");
+  }
+  // * GET `'/api/lessons/all'` - gets all lessons
+
+  newLesson(lesson: Lesson) {
+    return this.http.post("/api/lessons/new", lesson);
+  }
+  // * POST `'/api/lessons/new'` - creates a new lesson.   Requires cohortid, topicid, title, week, and day
+
+  editLesson(lesson: Lesson) {
+    return this.http.put("/api/lessons/edit", lesson);
+  }
+  // * PUT `'/api/lessons/edit'` - updates a lesson.  Requires cohortid, topicid, title, week, day, and the id of the lesson.
+
+  deleteLessson(id: number) {
+    return this.http.delete("/api/lessons/delete/"+id);
+  }
+  // * DELETE `'/api/lessons/delete/:id'` - deletes lesson with a given id
+
 }
