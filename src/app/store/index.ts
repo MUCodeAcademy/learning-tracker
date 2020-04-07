f8d5c941a642d07c62df3b74ef8584108412eea4
Mike <mike@miketruax.com>
2020-04-07 14:16:28 -0500

    fixes error with merge conflict

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,11 +2,11 @@ import * as Reducers from "./reducers";
 import { ActionReducerMap } from "@ngrx/store";
 
 export interface RootState {
-  user: Reducers.UserState;
-  lessons: Reducers.LessonState;
-  retentions: Reducers.RetentionState;
-  instructorQuestions: Reducers.InstructorQuestionState;
-  cohort: Reducers.cohortReducer
+  user: Reducers.UserState,
+  lessons: Reducers.LessonState,
+  retentions: Reducers.RetentionState,
+  instructorQuestions: Reducers.InstructorQuestionState,
+  cohort: Reducers.CohortState
 }
 
 export const reducers: ActionReducerMap<RootState> = {
