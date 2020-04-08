0a66d17d7c5916ca512eb973982eff773390aab9
Mike <mike@miketruax.com>
2020-04-08 09:44:34 -0500

    fixes minor import errors

--- src/app/store/selectors/index.ts
+++ src/app/store/selectors/index.ts
@@ -2,3 +2,4 @@ export * from "./user.selectors";
 export * from "./lesson.selectors";
 export * from "./instructorquestion.selectors";
 export * from './cohort.selectors';
+export * from './notes.selectors';
