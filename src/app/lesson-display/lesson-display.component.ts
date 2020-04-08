7a5374c67f84c8314458d462466d0a30ea4b123a
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-08 15:58:06 -0500

    Save Before Trying To Merge Master Changes Into My Branch

--- src/app/lesson-display/lesson-display.component.ts
+++ src/app/lesson-display/lesson-display.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { Cohort } from '../interfaces/Cohort.interface';
 import * as Selectors from '../store/selectors'
 import * as qclone from 'qclone'
+import { CohortService } from '../services/cohort.service';
 
 @Component({
   selector: 'app-lesson-display',
@@ -15,14 +16,20 @@ export class LessonDisplayComponent implements OnInit {
   cohortList$: Observable<any>
   cohortList: Array<Cohort>
 
-  constructor(private store: Store<RootState>) {
+  constructor(private store: Store<RootState>, private cohortService: CohortService) {
+    console.log('TODDTESTING1');
+    //this.cohortService.getAllCohorts();   // THIS ON IS CAUSING THE ERROR
+    console.log('TODDTESTING2');
     this.cohortList$ = this.store.select(Selectors.getCohortList);
+    console.log('TODDTESTING3');
    }
 
   ngOnInit(): void {
-    this.cohortList$.subscribe((res: Cohort[]) => {
-      this.cohortList = qclone.qclone(res)
-    })
+    console.log('TODDTESTING4');
+    // this.cohortList$.subscribe((res: Cohort[]) => {
+    //   this.cohortList = qclone.qclone(res)
+    // })
+    console.log('TODDTESTING');
   }
 
 }
