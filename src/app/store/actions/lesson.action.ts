a87364e035cfa7a7f79f5c14a28a356e7a48a37b
mikejunt <mikejunt@gmail.com>
2020-04-08 14:42:13 -0500

    updates lesson service

--- src/app/store/actions/lesson.action.ts
+++ src/app/store/actions/lesson.action.ts
@@ -1,6 +1,5 @@
 import {createAction, props} from '@ngrx/store';
 import { Lesson } from '../../interfaces/lesson.interface'
 
-export const setLessons = createAction('[LESSON STATE] Set Lessons', props<{lessons: Lesson}>());
-export const addLesson = createAction('[LESSON STATE] Add Lesson', props<{lesson: Lesson}>());
+export const setLessons = createAction('[LESSON STATE] Set Lessons', props<{lessons: Lesson[]}>());
 export const clearLessons = createAction('[LESSON STATE Clear Lessons');
