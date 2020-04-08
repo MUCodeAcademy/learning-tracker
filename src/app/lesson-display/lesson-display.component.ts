34b68c16ed4a559c27ebb643fc860c4bb2020638
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-08 13:18:24 -0500

    Initial Coding

--- src/app/lesson-display/lesson-display.component.ts
+++ src/app/lesson-display/lesson-display.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RootState } from '../store';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Cohort } from '../interfaces/Cohort.interface';
+import * as Selectors from '../store/selectors'
+import * as qclone from 'qclone'
 
 @Component({
   selector: 'app-lesson-display',
@@ -6,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-display.component.scss']
 })
 export class LessonDisplayComponent implements OnInit {
+  cohortList$: Observable<any>
+  cohortList: Array<Cohort>
 
-  constructor() { }
+  constructor(private store: Store<RootState>) {
+    this.cohortList$ = this.store.select(Selectors.getCohort);
+   }
 
   ngOnInit(): void {
+    this.cohortList$.subscribe((res: Cohort) => {
+      this.cohortList = qclone.qclone(res)
+    })
   }
 
 }
