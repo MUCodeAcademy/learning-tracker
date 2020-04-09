9e02664da858d7f8582f1cf965ba37da7593632c
mikejunt <mikejunt@gmail.com>
2020-04-09 14:53:48 -0500

    sets up subscription
    Co-authored-by: ToddAdelgren <toddad538@gmail.com>

--- src/app/lesson/lesson-display/lesson-display.component.ts
+++ src/app/lesson/lesson-display/lesson-display.component.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { RootState } from 'src/app/store';
 import * as Selectors from '../../store/selectors';
 import { Lesson } from 'src/app/interfaces/lesson.interface';
-import * as qclone from 'qclone';
 import * as Actions from '../../store/actions';
 
 @Component({
@@ -15,21 +14,24 @@ import * as Actions from '../../store/actions';
 export class LessonDisplayComponent implements OnInit {
   lessonList$: Observable<any>
   lessonList: Array<Lesson>
-  viewedLesson$: Observable<any>
-  viewedLesson: string
+  viewedLessonid$: Observable<any>
+  viewedLessonid: string
+  viewedLesson: Lesson
 
   constructor(private store: Store<RootState>) {
     this.store.dispatch(Actions.setViewedLesson({lessonid: "3"}));  // THIS IS FOR TESTING ONLY
     this.lessonList$ = this.store.select(Selectors.getLessons);
-    this.viewedLesson$ = this.store.select(Selectors.getViewedLesson);
+    this.viewedLessonid$ = this.store.select(Selectors.getViewedLesson);
    }
 
   ngOnInit(): void {
     this.lessonList$.subscribe((res: Lesson[]) => {
-      this.lessonList = qclone.qclone(res)
+      this.lessonList = res
     })
-    this.viewedLesson$.subscribe((res: string) => {
-      this.viewedLesson = qclone.qclone(res)
+    this.viewedLessonid$.subscribe((res: string) => {
+      this.viewedLessonid = res;
+      let target = this.lessonList.find(obj => {return obj.id === res})
+      this.viewedLesson = target
     })
   }
 
