fdf453d96a23cca8aa7d7a372a4a54bbd3be2aaf
mikejunt <mikejunt@gmail.com>
2020-04-09 12:29:38 -0500

    refactor menu

--- src/app/lesson/lesson-selection/lesson-selection.component.ts
+++ src/app/lesson/lesson-selection/lesson-selection.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RootState } from '../../store';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent, of, combineLatest, from } from 'rxjs';
 import { Cohort } from '../../interfaces/Cohort.interface';
 import * as Selectors from '../../store/selectors'
-import * as qclone from 'qclone'
+import * as Actions from '../../store/actions'
 import { CohortService } from '../../services/cohort.service';
 import { Lesson } from '../../interfaces/lesson.interface';
 import { LessonService } from '../../services/lesson.service';
@@ -19,21 +19,38 @@ export class LessonSelectionComponent implements OnInit {
   cohortList: Array<Cohort>
   lessonList$: Observable<any>
   lessonList: Array<Lesson>
+  userrole$: Observable<string>
+  userrole: string
+  selectedlesson: string
+  selectedcohort: string = ""
+  selectedcohort$: Observable<string> 
 
   constructor(private store: Store<RootState>, private cohortService: CohortService, private lessonService: LessonService) {
-    this.cohortService.getAllCohorts();   // THIS WILL BE REMOVED LATER
-    this.lessonService.getAllLessons();   // THIS WILL BE REMOVED LATER
     this.cohortList$ = this.store.select(Selectors.getCohortList);
     this.lessonList$ = this.store.select(Selectors.getLessons);
+    this.userrole$ = this.store.select(Selectors.getUserRole);
+    this.selectedcohort$ = of(this.selectedcohort)
    }
 
   ngOnInit(): void {
     this.cohortList$.subscribe((res: Cohort[]) => {
-      this.cohortList = qclone.qclone(res)
+      this.cohortList = res
+      console.log(res)
     })
     this.lessonList$.subscribe((res: Lesson[]) => {
-      this.lessonList = qclone.qclone(res)
+      this.lessonList = res
     })
+    this.userrole$.subscribe(res => this.userrole = res)
+    this.selectedcohort$.subscribe(res => console.log("from sub", res))
+  }
+
+  lessonChoice(id: string) {
+    this.store.dispatch(Actions.setViewedLesson({lessonid: id}))
+  }
+
+  cohortChoice(id: string){
+    this.selectedcohort = id
+    console.log("I changed it", id, this.selectedcohort)
   }
 
 }
