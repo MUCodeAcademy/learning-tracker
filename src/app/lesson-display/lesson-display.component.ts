f5d846a9da2489853f120f42ee6b54bafea938b7
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-08 19:03:48 -0500

    Save Before Push To Github Because Of Questions. I Did Not Do Merge Of Master Mike J Suggested

--- src/app/lesson-display/lesson-display.component.ts
+++ src/app/lesson-display/lesson-display.component.ts
@@ -6,6 +6,8 @@ import { Cohort } from '../interfaces/Cohort.interface';
 import * as Selectors from '../store/selectors'
 import * as qclone from 'qclone'
 import { CohortService } from '../services/cohort.service';
+import { Lesson } from '../interfaces/lesson.interface';
+import { LessonService } from '../services/lesson.service';
 
 @Component({
   selector: 'app-lesson-display',
@@ -15,15 +17,22 @@ import { CohortService } from '../services/cohort.service';
 export class LessonDisplayComponent implements OnInit {
   cohortList$: Observable<any>
   cohortList: Array<Cohort>
+  lessonList$: Observable<any>
+  lessonList: Array<Lesson>
 
-  constructor(private store: Store<RootState>, private cohortService: CohortService) {
-    this.cohortService.getAllCohorts();
+  constructor(private store: Store<RootState>, private cohortService: CohortService, private lessonService: LessonService) {
+    this.cohortService.getAllCohorts();   // THIS WILL BE REMOVED LATER
+    this.lessonService.getAllLessons();   // THIS WILL BE REMOVED LATER
     this.cohortList$ = this.store.select(Selectors.getCohortList);
+    this.lessonList$ = this.store.select(Selectors.getLessons);
    }
 
   ngOnInit(): void {
     this.cohortList$.subscribe((res: Cohort[]) => {
       this.cohortList = qclone.qclone(res)
+    })
+    this.lessonList$.subscribe((res: Lesson[]) => {
+      this.lessonList = qclone.qclone(res)
     })
   }
 
