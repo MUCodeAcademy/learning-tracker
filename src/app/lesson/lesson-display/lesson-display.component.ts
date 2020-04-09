80dd6c19ec0c35a56c3646148c99a141b0e33a96
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-09 14:38:16 -0500

    Saved Before Push

--- src/app/lesson/lesson-display/lesson-display.component.ts
+++ src/app/lesson/lesson-display/lesson-display.component.ts
@@ -4,7 +4,8 @@ import { Store } from '@ngrx/store';
 import { RootState } from 'src/app/store';
 import * as Selectors from '../../store/selectors';
 import { Lesson } from 'src/app/interfaces/lesson.interface';
-import * as qclone from 'qclone'
+import * as qclone from 'qclone';
+import * as Actions from '../../store/actions';
 
 @Component({
   selector: 'app-lesson-display',
@@ -15,9 +16,10 @@ export class LessonDisplayComponent implements OnInit {
   lessonList$: Observable<any>
   lessonList: Array<Lesson>
   viewedLesson$: Observable<any>
-  viewedLesson: number
+  viewedLesson: string
 
   constructor(private store: Store<RootState>) {
+    this.store.dispatch(Actions.setViewedLesson({lessonid: "3"}));  // THIS IS FOR TESTING ONLY
     this.lessonList$ = this.store.select(Selectors.getLessons);
     this.viewedLesson$ = this.store.select(Selectors.getViewedLesson);
    }
@@ -26,7 +28,7 @@ export class LessonDisplayComponent implements OnInit {
     this.lessonList$.subscribe((res: Lesson[]) => {
       this.lessonList = qclone.qclone(res)
     })
-    this.viewedLesson$.subscribe((res: number) => {
+    this.viewedLesson$.subscribe((res: string) => {
       this.viewedLesson = qclone.qclone(res)
     })
   }
