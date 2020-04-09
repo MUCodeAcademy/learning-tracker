843d2c75475f991130a7d96fbf3bcfc19ee66edb
Jacqueline Mezick <jmezick@Jacquelines-MacBook-Air.local>
2020-04-09 11:40:19 -0500

    Initial Save Question Functionality
    Co-authored-by: Mike Truax <mike@miketruax.com>
    Co-authored-by: Mike Juntunen <mikejunt@gmail.com>

--- src/app/instructor-question-list/instructor-question-list.component.ts
+++ src/app/instructor-question-list/instructor-question-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { InstructorQuestion } from "../interfaces/instructorquestion.interface";
+import { Store } from "@ngrx/store";
+import { RootState } from "../store/";
+import { QuestionsService } from "../services/questions.service";
+import * as Selectors from "../store/selectors";
+import * as qclone from "qclone";
+import { User } from "../interfaces/user.interface";
 
 @Component({
   selector: "app-instructor-question-list",
@@ -6,6 +14,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./instructor-question-list.component.scss"],
 })
 export class InstructorQuestionListComponent implements OnInit {
+  userid$: Observable<string>;
+  userid;
+  instructorQuestion$: Observable<InstructorQuestion[]>;
+  instructorQuestion: InstructorQuestion[];
+  viewedlesson$: Observable<string>;
+  viewedlesson: string;
+  instructor$: Observable<string>;
+  instructor: string;
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/instructor-question/instructor-question.component.ts
+++ src/app/instructor-question/instructor-question.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { InstructorQuestion } from "../interfaces/instructorquestion.interface";
+import { Store } from "@ngrx/store";
+import { RootState } from "../store/";
+import { QuestionsService } from "../services/questions.service";
+import * as Selectors from "../store/selectors";
+import * as qclone from "qclone";
+import { User } from "../interfaces/user.interface";
 
 @Component({
   selector: "app-instructor-question",
@@ -6,7 +14,69 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./instructor-question.component.scss"],
 })
 export class InstructorQuestionComponent implements OnInit {
-  constructor() {}
+  userid$: Observable<string>;
+  userid;
+  userrole$: Observable<string>;
+  userrole: string;
+  instructorQuestion$: Observable<InstructorQuestion[]>;
+  instructorQuestion: InstructorQuestion[];
+  viewedlesson$: Observable<string>;
+  viewedlesson: string;
+  instructor$: Observable<string>;
+  instructor: string;
 
-  ngOnInit(): void {}
+  newinstructorQuestion: InstructorQuestion = {
+    id: "",
+    student_id: "",
+    instructor_id: "",
+    lesson_id: "",
+    question_text: "",
+    question_answer: "",
+  };
+
+  questionTemplate: InstructorQuestion = {
+    id: "",
+    student_id: "",
+    instructor_id: "",
+    lesson_id: "",
+    question_text: "",
+    question_answer: "",
+  };
+
+  constructor(
+    private store: Store<RootState>,
+    private questions: QuestionsService
+  ) {
+    this.instructorQuestion$ = this.store.select(
+      Selectors.getInstructorQuestions
+    );
+    this.userid$ = this.store.select(Selectors.getUserId);
+  }
+
+  saveQuestion(question: InstructorQuestion) {
+    if (question.id != "" || 0) {
+      this.questions.editQuestion(
+        question.id,
+        question.question_text,
+        question.question_answer
+      );
+      // this is an edit and we will call the edit function
+    }
+    if (question.id === "") {
+      // this is a new question and we must configure it
+      question.student_id = this.userid;
+      question.lesson_id = this.viewedlesson;
+      question.instructor_id = this.instructor;
+      this.questions.questionQuestion(question);
+      this.newinstructorQuestion = { ...this.questionTemplate };
+    }
+  }
+
+  ngOnInit(): void {
+    this.instructorQuestion$.subscribe((res) => {
+      let data = qclone.qclone(res);
+      this.instructorQuestion = data;
+    });
+    this.userid$.subscribe((res) => (this.userid = res));
+  }
 }
