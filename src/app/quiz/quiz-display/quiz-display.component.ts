17ffc481ac883b9437419ef0f01d92e8afbc3fcd
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-04-10 10:32:06 -0500

    My Bad Try It Now

---
+++ src/app/quiz/quiz-display/quiz-display.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, combineLatest } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { RootState } from 'src/app/store';
+import * as Selectors from '../../store/selectors';
+import { map } from 'rxjs/operators';
+import { Quiz } from 'src/app/interfaces/quiz.interface';
+
+@Component({
+  selector: 'app-quiz-display',
+  templateUrl: './quiz-display.component.html',
+  styleUrls: ['./quiz-display.component.scss']
+})
+export class QuizDisplayComponent implements OnInit {
+  quizList$: Observable<any>
+  quizList: Array<Quiz>
+  viewedQuiz$: Observable<any>
+  viewedQuiz: string
+  viewedQuizTarget: Quiz
+
+  constructor(private store: Store<RootState>) {
+    this.quizList$ = this.store.select(Selectors.getQuiz);
+    this.viewedQuiz$ = this.store.select(Selectors.getViewedQuiz);
+  }
+
+  ngOnInit(): void {
+
+    combineLatest([this.quizList$, this.viewedQuiz$]).pipe(
+      map(([list, viewedId]) => ({list, viewedId}))
+  )
+  .subscribe(pair => {
+      this.quizList = pair.list as Quiz[]
+      this.viewedQuiz = pair.viewedId;
+      let target = this.quizList.find(obj => { return obj._id === pair.viewedId })
+      this.viewedQuizTarget = target
+  })
+  }
+}
