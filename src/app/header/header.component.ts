f22d7234a121dfd380711e0a2128409f279acaa0
Jblobaum <justin.blobaum@midlandu.edu>
2020-04-10 16:07:10 -0500

    fixed ngIf's and updated header to reflect roles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user.interface';
+import { Store } from '@ngrx/store';
+import { RootState } from '../store';
+import * as Selectors  from '../store/selectors'
 
 @Component({
   selector: 'app-header',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user$: Observable<any>
+  user: User = {
+    first_name: "",
+    last_name: "",
+    role_id: "4",
+    id: "",
+    email_address: ""
+  }
+  constructor(
+    private store: Store<RootState>
+  ) {
+    this.user$ = this.store.select(Selectors.getUserInfo)
+   }
 
   ngOnInit(): void {
+    this.user$.subscribe((res: User) => {
+      this.user = res
+    })
+
+    console.log(this.user);
+    
   }
 
 }
